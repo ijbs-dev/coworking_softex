09f53c2f43081359c0bc73247f7caa9e1957d5ef
Walter Andrade <65465090+walterwa@users.noreply.github.com>
2024-01-10 00:21:43 -0300

    atualização do SWAGGER de claudio. Implementação apenas de USUARIO - corrigir os métodos PUT e DELETE

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,17 @@
 import "reflect-metadata";
 import express from "express";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
 import { AppDataSource } from "./database/data-source";
 import { routes } from "./app/routes/routes";
-
+import swaggerDocs from "./swagger.json";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 app.use(routes);
 
