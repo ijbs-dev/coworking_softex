c373126979bd36acf9814fe6505116d3c89be345
Walter Andrade <65465090+walterwa@users.noreply.github.com>
2024-01-15 11:27:37 -0300

    atualização package.json com mods twilio e nodemailer, criação de controllers para email e whatsApp

--- src/app/routes/recebimentoEncomenda.routes.ts
+++ src/app/routes/recebimentoEncomenda.routes.ts
@@ -3,6 +3,8 @@ import { RecebimentoEncomendaRepository } from "../repositories/RecebimentoEncom
 import { RecebimentoEncomendaController } from "../controllers/RecebimentoEncomendaController";
 import { EncomendaRepository } from "../repositories/EncomendaRepository";
 import { autenticacao } from "../middleware/autenticacao";
+import { sendWhatsAppMessage } from "../controllers/WhatsAppController";
+import { transporter } from "../controllers/EmailController";
 
 const recebimentoEncomendaRoutes = Router();
 const recebimentoEncomendaRepository = new RecebimentoEncomendaRepository();
@@ -47,7 +49,31 @@ recebimentoEncomendaRoutes.get("/id/:id", async (request, response) => {
 
 recebimentoEncomendaRoutes.post("/", async (request, response) => {
     const { obsEncomenda, obsRecebEncomenda, numEndFiscal, idRecepcao } = request.body;
+  
+    const emailRepresentante = `rixele_praxedes@email.com`;
+        
+    //configurando email
+    const mailOptions = {
+        from: 'softex@email.com',
+        to: emailRepresentante,
+        subject: 'Encomenda Recebida na Softex',
+        text: obsEncomenda
+      };
 
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error(error);
+          response.status(500).send('Erro ao enviar e-mail');
+        } else {
+          console.log('E-mail enviado: ' + info.response);
+          response.status(200).send(`Encomenda registrada com sucesso e e-mail enviado!`);
+        }
+      });   
+
+     //Envio WhatsApp
+        const mensagemPadrao = `Nova encomenda recebida para você: ${obsEncomenda}`;
+        await sendWhatsAppMessage(mensagemPadrao);
+      
     try {
         await autenticacao(request, response, () => {});
 
